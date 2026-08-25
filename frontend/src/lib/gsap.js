7108ef15bccc8f9adbc3ab439f7377b76f85d3a5
DivyamOswal <divyam9850400550@gmail.com>
2026-08-25 11:14:20 +0530

    Add GSAP and ScrollTrigger support; create Reveal and SmoothScroll components

---
+++ frontend/src/lib/gsap.js
@@ -0,0 +1,8 @@
+// frontend/src/lib/gsap.js
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
+
+export { gsap, ScrollTrigger, useGSAP };
