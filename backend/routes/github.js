623673d71276eae6a5d0115b333522c03c25f635
DivyamOswal <divyam9850400550@gmail.com>
2026-09-10 13:19:23 +0530

    Remove unused applyAIFix route from GitHub API routes

--- backend/routes/github.js
+++ backend/routes/github.js
@@ -3,7 +3,7 @@ import {
   analyzeGithubRepo,
   autoFixIssue,
   generateTestCases,
-  getRepoContents, getFileContent, applyAIFix 
+  getRepoContents, getFileContent 
 } from "../controllers/githubController.js";
 import protect from "../middleware/authMiddleware.js";
 import auth from "../middleware/authMiddleware.js";
@@ -18,6 +18,5 @@ router.post("/generate-tests", protect, generateTestCases);
 router.post("/auto-fix", auth, autoFixIssue);
 router.get('/repo/contents', protect, getRepoContents);
 router.get('/repo/file', protect, getFileContent);
-router.post('/repo/fix', protect, applyAIFix);
 
 export default router;
